e118f25448f09cceae566db38071566a76241c8f
Dekiru <dwyl.agency1@gmail.com>
2026-03-08 17:02:20 +0530

    routes.py

--- view_todo/routes.py
+++ view_todo/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, url_for, Blueprint
+from flask import render_template, request, url_for, Blueprint, redirect
 from blueprint.app import db
 from blueprint.study_todo.models import study
 
@@ -8,3 +8,10 @@ view_todo = Blueprint('view_todo',__name__, template_folder='templates')
 def view():
     todo_view = study.query.all()
     return render_template("view_todo/view.html", todo_view=todo_view)
+
+@view_todo.route("/delete/<pid>")
+def delete(pid):
+    study.query.filter(study.pid == pid).delete()
+
+    db.session.commit()
+    return redirect(url_for("view_todo.view"))
